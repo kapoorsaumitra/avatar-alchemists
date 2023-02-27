7a44a10e14d1356300aba3a70959cb2d5a63cd11
Saumitra Kapoor <saumitrakapoor24@gmail.com>
2023-02-27 12:42:10 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import buildspaceLogo from '../assets/buildspace-logo.png';
+
+const Home = () => {
+  return (
+    <div className="root">
+      <Head>
+        <title>Avatar Alchemists</title>
+      </Head>
+      <div className="container">
+        <div className="header">
+          <div className="header-title">
+            <h1>avatar alchemists</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>where pixels and personality come to play!</h2>
+          </div>
+        </div>
+      </div>
+      <div className="badge-container grow">
+        <a
+          href="https://buildspace.so/builds/ai-avatar"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="badge">
+            <Image src={buildspaceLogo} alt="buildspace logo" />
+            <p>build with buildspace</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
